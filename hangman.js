aa143d81e1ca56351a97ef05cc729b49cd33d956
Karen Nemeth <karen61990@gmail.com>
2016-09-12 15:08:44 +0200

    added some styling

--- hangman.js
+++ hangman.js
@@ -1,3 +1,45 @@
 (function() {
 
+//initializing game
+    // var initializeGame = document.getElementById("ready");
+    // var opener = document.getElementById("opener");
+    //
+    // initializeGame.addEventListener("click", function() {
+    //     opener.style.display="none";
+    // });
+
+//gameplay
+
+    var words = ["gandalf", "liked", "to", "smoke", "his", "pipe"];
+
+    function chooseWord() {
+
+    }
+
+
+
+//input fields
+  //   var userInput = document.getElementById("userInput").value;
+  //
+  //   var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
+  //       "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+  //       "t", "u", "v", "w", "x", "y", "z"];
+  //   var alphabetContainer = document.getElementById("alphabetContainer");
+  //   var alphabetList;
+  //   var letters;
+  //
+  //
+  //   function lettersToSelect() {
+  //       alphabetList = document.createElement("ul");
+  //       for (var i = 0; i < alphabet.length; i++) {
+  //           letters = document.createElement("li");
+  //           letters.id = "letters";
+  //           letters.innerHTML = alphabet[i];
+  //           alphabetContainer.appendChild(alphabetList);
+  //           alphabetList.appendChild(letters);
+  //       }
+  //     }
+  //
+  // lettersToSelect();
+
 })();

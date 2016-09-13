e3bbcb851e3e9ba035f2542c4de9a325a13578c6
Karen Nemeth <karen61990@gmail.com>
2016-09-13 11:19:03 +0200

    trying text area

--- hangman.js
+++ hangman.js
@@ -8,38 +8,59 @@
     //     opener.style.display="none";
     // });
 
+    //Add in starting the game
+
+//input fields
+    function inputs() {
+
+        //letter guesses
+        var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H",
+            "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+            "T", "U", "V", "W", "X", "Y", "Z"];
+        var alphabetContainer = document.getElementById("alphabetContainer");
+        var alphabetList;
+        var letters;
+
+        function lettersToSelect() {
+            alphabetList = document.createElement("ul");
+            alphabetList.id = "alphabetList";
+            for (var i = 0; i < alphabet.length; i++) {
+                letters = document.createElement("li");
+                letters.className += "letters";
+                letters.innerHTML = alphabet[i];
+                alphabetContainer.appendChild(alphabetList);
+                alphabetList.appendChild(letters);
+            }
+        }
+
+        lettersToSelect();
+
+        //full word guesses
+        var userEnteredWords = [];
+        var wordInput = document.getElementById("wordInput");
+        var fullWordContainer = document.getElementById("fullWordGuesses");
+        var wordButton = document.getElementById("wordButton");
+        var word;
+
+        wordButton.addEventListener("click", function() {
+            var userInputWord = wordInput.value;
+            userInputWord.push(userEnteredWords);
+            word = document.createTextNode(userEnteredWords);
+            word.push(fullWordContainer);
+        });
+    }
+
+    inputs();
+
+
 //gameplay
 
-    var words = ["gandalf", "liked", "to", "smoke", "his", "pipe"];
+    var words = ["gandalf", "likes", "smoking", "pipeweed", "with", "hobbits"];
 
     function chooseWord() {
-
+        var chosenWord = words[Math.floor(Math.random() * words.length)];
     }
 
 
-
-//input fields
-  //   var userInput = document.getElementById("userInput").value;
-  //
-  //   var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
-  //       "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-  //       "t", "u", "v", "w", "x", "y", "z"];
-  //   var alphabetContainer = document.getElementById("alphabetContainer");
-  //   var alphabetList;
-  //   var letters;
-  //
-  //
-  //   function lettersToSelect() {
-  //       alphabetList = document.createElement("ul");
-  //       for (var i = 0; i < alphabet.length; i++) {
-  //           letters = document.createElement("li");
-  //           letters.id = "letters";
-  //           letters.innerHTML = alphabet[i];
-  //           alphabetContainer.appendChild(alphabetList);
-  //           alphabetList.appendChild(letters);
-  //       }
-  //     }
-  //
-  // lettersToSelect();
 
 })();
